20f6088486921433adbb55da7286291956c45e8e
GPS-TRACHER <dmuhi9959@gmail.com>
2026-09-19 23:39:47 +0700

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'display-sekolah-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS).catch(function() {
+        return Promise.resolve();
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if (response) return response;
+      return fetch(e.request).then(function(resp) {
+        if (resp && resp.status === 200 && resp.type === 'basic') {
+          var respClone = resp.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(e.request, respClone);
+          });
+        }
+        return resp;
+      }).catch(function() {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) {
+          return key !== CACHE_NAME;
+        }).map(function(key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
